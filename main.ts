9390f745f6deae2bc4a521dbc3b63625dd32317e
Lekesova <155728987+Lekesova@users.noreply.github.com>
2024-04-20 12:40:40 +0200

    Počáteční soubory projektu MakeCode

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Klara")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Arnost")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Lekesova")
+})
